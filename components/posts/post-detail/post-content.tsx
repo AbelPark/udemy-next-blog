78cda7c7ccb041a2deb2c3a01101b41b8cfcb415
AbelPark <56255317+AbelPark@users.noreply.github.com>
2022-12-19 16:14:20 +0900

    Contact 폼 작성하기

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,15 +1,49 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function PostContent(props: any) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    p(paragraph: any) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code(code: any) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; //
+
+      return (
+        <SyntaxHighlighter language={language} style={atomDark}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -5,28 +5,28 @@ export default function PostDetailPage(props: any) {
   return <PostContent post={props.post} />;
 }
 
-export function getStaticProps(context: any) {
-  const { params } = context;
-  const { slug } = params;
-
-  const postData = getPostData(slug);
-  return {
-    props: {
-      post: postData,
-    },
-    revalidate: 600, // 마크다운이 업데이트 될 때마다 최신 데이터 가져옴(10분)
-  };
-}
-
 export function getStaticPaths() {
   const postFilenames = getPostsFiles();
-  console.log(postFilenames);
-
   const slugs: any = postFilenames.map((filename: any) =>
     filename.replace(/\.md$/, "")
   );
   return {
     paths: slugs.map((slug: any) => ({ params: { slug: slug } })),
     fallback: false,
+  };
+}
+
+export function getStaticProps(context: any) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+  console.log(postData);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600, // 마크다운이 업데이트 될 때마다 최신 데이터 가져옴(10분)
   };
 }
